51ab13a43eceb0c72cfffc328ff5ad67421892bc
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-27 18:30:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line
+  if count <= 1 
+    puts 
+  
+  elsif count = 0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, << name)
+  puts [1][0]
+end
+
+def now_serving
+  puts take_a_number[1].shift
+  if take_a_number = 0 
+    puts "There is nobody waiting to be served!"
+  end
+end
